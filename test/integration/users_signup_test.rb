2e38d4ce193b9f6fc7f8c31c658a01a39141cab9
Valentin Meica <vanjinn@gmail.com>
2016-04-22 19:26:49 +0000

    ch7 excecise 123

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -22,6 +22,7 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
                                             password_confirmation: "password" }
     end
     assert_template 'users/show'
+     assert_not_empty flash
   end
   
   
